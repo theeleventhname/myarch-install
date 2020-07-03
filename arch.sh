9cefd09eb9ac21f58f6b6786c144d96e1ad86d28
theeleventhname <31616713+theeleventhname@users.noreply.github.com>
2020-07-03 18:40:29 +0800

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -8,17 +8,27 @@ read wipedisk
 
 fdisk /dev/$wipedisk
 
+echo -e "which type is your device?\n1.uefi 2.mbr or virtualbox"
+read uefi
+
+
 lsblk
 echo "input boot disk"
 read boot
 echo "input root disk"
 read root 
-mkfs.fat -F32 /dev/$boot
 mkfs.ext4 /dev/$root
-
 mount /dev/$root /mnt
+if [ $uefi -eq 1 ];then
+mkfs.fat -F32 /dev/$boot
 mkdir -p /mnt/boot/efi
 mount /dev/$boot /mnt/boot/efi
+else
+mkfs.ext4 /dev/$boot
+mkdir -p /mnt/boot
+mount /dev/$boot /mnt/boot
+fi
+
 
 sed -i "7i# 163\nServer = http://mirrors.163.com/archlinux/\$repo/os/\$arch\n## aliyun\nServer = http://mirrors.aliyun.com/archlinux/\$repo/os/\$arch\n# 清华大学\nServer = https://mirrors.tuna.tsinghua.edu.cn/archlinux/\$repo/os/\$arch\n" /etc/pacman.d/mirrorlist
 pacman -Syy
